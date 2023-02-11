22b4d4119e6692479cb0c5e396ae6378337898eb
Vinay Kumar <Vinayy1709@gmail.com>
2023-02-11 18:20:11 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header"
+import Footer from "./Footer"
 
 function App(){
   return <div>
       <Header></Header>
+      <Footer></Footer>
   </div>
 }
 
